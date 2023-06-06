023225d1d3d7d94f1e0c728a34ad67a1b9a7a921
Jordan Fudge <jordanfudge95@gmail.com>
2023-06-06 17:32:39 -0400

    Added Context

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import { DataContext } from "../Contexts/DataContext";
 import GalleryItem from "./GalleryItem";
 
-function Gallery({ data }) {
+function Gallery({}) {
+  const data = useContext(DataContext);
   const display = data.map((item, i) => {
     return <GalleryItem key={i} item={item} />;
   });
@@ -9,5 +12,6 @@ function Gallery({ data }) {
 
 export default Gallery;
 
+// We've imported the Hook { useContext } and we've imported the Context itself { DataContext }
 // The Gallery , much like our App.js, is a wrapper.
 // i - index
